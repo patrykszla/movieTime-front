cb33d9468a4389f282fcaee106cf696f23823c34
Patryk Szlachta <patryk.szla@interia.pl>
2021-02-17 21:45:30 +0100

    working with film details

--- src/views/FilmDetails.vue
+++ src/views/FilmDetails.vue
@@ -5,26 +5,51 @@
             <div class="movie-image-title"  v-bind:style="{ backgroundImage: 'url(' + movie.image + ')' }">
                 <h1 class="movie-title movie-title-big">{{ movie.title }}</h1>
             </div>
-            <div class="movie-description-wrapper">
+            <div class="film-details-wrapper">
                 <div class="movie-icon" v-bind:style="{ backgroundImage: 'url(' + movie.image2 + ')' } ">
                     
                 </div> 
                 <div class="movie-description">
                     <h3 class='movie-title movie-title-small'>{{ movie.title }}</h3>
                     <div class="description">
-                        <p>Opis:</p>
+                        <p class="paragraph">Opis:</p>
+                        <div class="description-wrapper">
+                            {{ movie.content }}
+                        </div>
                     </div>
+
                     <div class="type-creator-wrapper">
                         <div class="type">
-                            <p>Typ:</p>
+                            <p class="paragraph" >Typ:</p>
+                            <div class="type-wrapper">
+                                <div class="rectangle rectangle-gold type">{{ movie.type }}</div>
+                            </div>                        
                         </div>
                         
-                        <div class="creator">
-                            <p>Twórcy:</p>
+                        <div class="creators-wrapper">
+                            <p class="paragraph">Twórcy:</p>
+                            <div class="creators">
+                            <div class="rectangle rectangle-grey " v-for="creator in movie.creators" :key="creator">{{ creator}}</div>
+                            </div>
                         </div>
+                    </div>
+
+                    <div class="grades-wrapper">
+                        <p class="paragraph">Ocena:</p>
+                        <div class="grade">
+                            {{ movie.grade }}
+                        </div>
+                        <p class="paragraph">Twoja Ocena:</p>
+                        <div class="your-grade">
+
+                        </div>
+                        <div class="favorite-film">
+
+                        </div>
+                    </div>
 
                         
-                    </div>
+                    
                     
                 </div>
             </div>
@@ -45,6 +70,8 @@ export default {
                 title: 'Gambit królowej',
                 content: 'Amerykański stan Kentucky, lata 50. XX wi Mieszkająca w sierocińcu dziewczyna odkrywa swój wielki talent szachowy Jednocześnie musi walczyć z uzależnieniem',
                 grade: '33%',
+                creators: ['adas', 'andrzej'],
+                type: 'Dramat',
                 image: 'https://ocdn.eu/pulscms-transforms/1/2dxk9kpTURBXy8zOGNmNmE0YzQyMDNkNjYzZTFjZDYyNTljMjU1ZmNhYS5qcGeTlQMVAM0D1c0CKJMFzQMUzQG8kwmmYWUzMzYyBoGhMAE/marcin-dorocinski-i-anya-taylorjoy-na-planie-serialu-gambit-krolowej.jpg',
                 image2:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTujczsEwfaILjjZG0PgPrTU8EGorcd-Xn22g&usqp=CAU'
             }]}
@@ -79,14 +106,18 @@ export default {
     .movie-title {
     text-transform: uppercase;
     }
-    .movie-description-wrapper {
+    .film-details-wrapper {
         width: 100%;
         height: auto;
         display: flex;
         flex-wrap:nowrap;
         flex-direction: row;
         box-sizing: content-box;
+        text-align: left;
 
+        .paragraph {
+            text-align: left;
+        }
         .movie-icon {
             width: 33%;
             height: 200px;
@@ -97,17 +128,42 @@ export default {
         .movie-description {
             width: 66%;
 
+            
             .type-creator-wrapper {
                 display: flex;
                 flex-wrap: nowrap;     
                 
                 .type {
                     width: 50%;
-
+                    
+                    .rectangle {
+                        text-align: center;
+                        padding: 5px;
+                        // margin: 10px;
+                        // width: auto;
+                        // margin: auto;
+                        background-color: rgba(255,194,0, 0.8);
+                        color:#ce9c00;
+                        opacity: 0.7;
+                        border-radius: 4px;
+                    } 
                 }       
 
-                .creator    {
-                    width:50%;
+                .creators    {
+                    display:flex;
+                    flex-direction: row;
+                    // width:50%;
+                    .rectangle-grey {
+                        text-align: center;
+                        padding: 5px;
+                        margin-right: 15px;
+                        // width: auto;
+                        // margin: auto;
+                        background-color: rgba(51,51,51,0.8);
+                        color:#333333;
+                        opacity: 0.7;
+                        border-radius: 4px;
+                    } 
                 }   
             }
         }
